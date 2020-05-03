6b4c58e149fea6909f2d5ffc7a81e4ac44ed36ba
reikyDM <reikydemanto@gmail.com>
2020-05-03 10:02:47 +0700

    Splash Screen on proses 90%

--- app/src/main/java/com/example/biodatadiri/SplashScreen.kt
+++ app/src/main/java/com/example/biodatadiri/SplashScreen.kt
@@ -1,12 +1,24 @@
 package com.example.biodatadiri
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        Handler().postDelayed(
+            {
+                val intent = Intent(this,Registrasi::class.java)
+                startActivity(intent)
+                finish()
+            },
+            1000
+        )
     }
 }
